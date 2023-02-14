3c255391d1f10727c26e517ff9ac9fb7dca31ffe
Shubh-Raj <60479828+Shubh-Raj@users.noreply.github.com>
2023-02-15 03:03:26 +0530

    Update Arch.sh

--- Arch.sh
+++ Arch.sh
@@ -10,35 +10,66 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# Check if the system uses UEFI or BIOS
+if [[ -d /sys/firmware/efi/efivars ]]; then
+    echo "UEFI system detected"
+    BOOT_TYPE="UEFI"
+else
+    echo "BIOS system detected"
+    BOOT_TYPE="BIOS"
+fi
+
+echo "Detected boot mode: $BOOT_TYPE"
+
 # Update system clock
 timedatectl set-ntp true
 
 # Partition the disk (replace /dev/sda with the appropriate device)
-(
-  echo g      # create a new empty GPT partition table
-  echo n      # add a new partition
-  echo        # default partition number (1)
-  echo        # default start sector (2048)
-  echo +512M  # partition size
-  echo EF00   # partition type (EFI system)
-  echo n      # add a new partition
-  echo        # default partition number (2)
-  echo        # default start sector
-  echo        # default end sector (entire disk)
-  echo        # default partition type (Linux filesystem)
-  echo w      # write changes
-) | fdisk /dev/sda
+if [ "$BOOT_TYPE" = "UEFI" ]; then
+    (
+      echo g      # create a new empty GPT partition table
+      echo n      # add a new partition
+      echo        # default partition number (1)
+      echo        # default start sector (2048)
+      echo +512M  # partition size
+      echo EF00   # partition type (EFI system)
+      echo n      # add a new partition
+      echo        # default partition number (2)
+      echo        # default start sector
+      echo        # default end sector (entire disk)
+      echo        # default partition type (Linux filesystem)
+      echo w      # write changes
+    ) | fdisk /dev/sda
+	
+	# Format the partitions
+	mkfs.fat -F32 /dev/sda1
+	mkfs.ext4 /dev/sda2
+	
+	# Mount the root partition
+	mount /dev/sda2 /mnt
 
-# Format the partitions
-mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sda2
+	# Mount the EFI partition
+	mkdir -p /mnt/boot/efi
+	mount /dev/sda1 /mnt/boot/efi
+	
+else
+    (
+      echo o      # create a new empty DOS partition table
+      echo n      # add a new partition
+      echo p      # primary partition
+      echo        # default partition number (2)
+      echo        # default start sector
+      echo        # default end sector (entire disk)
+      echo w      # write changes
+    ) | fdisk /dev/sda
+	
+	# Format the partitions
+	mkfs.ext4 /dev/sda1
+	
+	# Mount the root partition
+	mount /dev/sda1 /mnt
+fi
 
-# Mount the root partition
-mount /dev/sda2 /mnt
-
-# Mount the EFI partition
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
 
 # Install base packages and kernel
 pacstrap /mnt base base-devel linux linux-firmware
@@ -87,10 +118,29 @@ echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 # Install nano text editor
 pacman -S nano
 
-# Install and configure GRUB for UEFI
-pacman -S grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
-grub-mkconfig -o /boot/grub/grub.cfg
+if [ "$BOOT_TYPE" = "UEFI" ]; then
+	(
+	# Install and configure GRUB for UEFI
+	pacman -S grub efibootmgr
+	grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
+	grub-mkconfig -o /boot/grub/grub.cfg
+	)
+	
+else
+	(
+	# Install GRUB
+	pacman -S grub
+
+	# Install GRUB to the boot partition
+	grub-install --target=i386-pc --boot-directory=/boot /dev/sda
+
+	# Generate GRUB configuration file
+	grub-mkconfig -o /boot/grub/grub.cfg
+
+	)
+fi
+
+
 
 EOF
 
